f67cc209354917c2e35f8fd5ef87cb3eb9c572ee
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2021-05-29 02:51:59 -0400

    Update CheckInScreen.js

--- screens/CheckInScreen.js
+++ screens/CheckInScreen.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, Button, TouchableOpacity, TextInput } from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome5";
 
-function CheckInScreen() {
+export default function CheckInScreen( navigation) {
+
+  const [water, setWater] = useState('10');;
+  const [exercise, setExercise] = useState('50');
+
+
   return (
     <View style = {styles.background}>
       <Text style={styles.headline}>Daily Check-In</Text>
@@ -30,24 +35,48 @@ function CheckInScreen() {
           </TouchableOpacity>
       </View>
 
-       <View style={styles.space} />
-       <Text style={styles.title}>Water intake</Text>
-       <View style={styles.space} />
+        <View style={styles.space} />
+        <Text style={styles.title}>Water intake</Text>
+        <View style={styles.space} />
 
-       <Text style={styles.title}>Amount of exercise</Text>
-       <View style={styles.space} />
+        <TextInput 
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder='e.g. 2.5'
+          onChangeText={()=> setWater}/>
 
-       <Text style={styles.title}>What did you do today?</Text>
-       <View style={styles.space} />
+        <Text style={styles.title}>Amount of exercise</Text>
+        <View style={styles.space} />
 
-       <Text style={styles.title}>Remainder of today's goals or plans:</Text>
-       <View style={styles.space} />
+        <TextInput 
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder='e.g. 3'
+          onChangeText={()=> setExercise()}/>
+
+        <Text style={styles.title}>What did you do today?</Text>
+        <View style={styles.space} />
+
+        <TextInput 
+          multiline
+          style={styles.input2}
+          placeholder='e.g. 3'
+          onChangeText={()=> setExercise()}/>
+
+        <Text style={styles.title}>Remainder of today's goals or plans:</Text>
+        <View style={styles.space} />
+
+        <TouchableOpacity onPress={() => navigation.navigate('OverviewScreen')}>
+              <Icon name="sign-in-alt" size={60} color="#b1ea6f" />
+          </TouchableOpacity>
 
     </View>
   );
 }
 
-export default CheckInScreen;
+
+
+// export default CheckInScreen;
 
 
 const styles = StyleSheet.create({
@@ -70,4 +99,19 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
   },
+  input:{
+    borderWidth:1,
+    borderColor: '#777',
+    padding: 8,
+    margin:10,
+    width:200,
+  },
+  input2:{
+    borderWidth:1,
+    borderColor: '#777',
+    padding: 8,
+    margin:10,
+    width:200,
+    height:150,
+  }
 });
