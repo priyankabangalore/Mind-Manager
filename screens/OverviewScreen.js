e720a714337e4e79e94bba938b556ec6d8070594
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2021-05-28 23:46:01 -0400

    Update OverviewScreen.js

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 function OverviewScreen() {
   return (
-    <View>
-        <Text>Overview</Text>
+    <View style={styles.background}>
+        <View style={styles.backgroundLayer1} >
+            <Text style={styles.title}>
+                Overview
+            </Text>
+        </View>
+
+        <View style={styles.backgroundLayer2} >
+            <View style={styles.categories}>
+                <Text style={styles.category}>
+                    Mood:
+                    <TouchableOpacity style={styles.iconFormat}>
+                        <Icon name="laugh-beam" size={40} color="#9fce68" />
+                    </TouchableOpacity>
+                </Text>
+                
+                <Text style={styles.category}>
+                    Water:
+                </Text>
+                <Text style={styles.category} >
+                    Exercise:
+                </Text>
+            </View>
+        </View>
+
+    
     </View>
   );
 }
@@ -13,5 +38,33 @@ export default OverviewScreen;
 
 
 const styles = StyleSheet.create({
-
+    background:{
+        flex:1,
+    },
+    backgroundLayer1:{
+        flex:1,
+        backgroundColor: "#C7CEF4",
+        alignItems: 'center',
+    },
+    backgroundLayer2:{
+        flex:8,
+        backgroundColor: "#DCA3C2"
+        
+    },
+    title:{
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginTop: 15,
+    },
+    categories:{
+        marginTop: 30,
+        left:30,
+    },
+    category:{
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    iconFormat:{
+        paddingLeft: 100,
+    }
 });
