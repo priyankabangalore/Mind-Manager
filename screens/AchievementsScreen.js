d77d12c362b89c82d2afd3c455841768451eb011
Sashintha Tennakoon <stennak@uwo.ca>
2021-05-29 03:33:23 -0400

    update stuff

--- screens/AchievementsScreen.js
+++ screens/AchievementsScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function AchievementScreen() {
   return (
-    <View>
-        <Text>Achievement</Text>
-    </View>
+    <LinearGradient style= {styles.gradient} colors = {['#C7CEF4', '#DCA3C2']}>
+      <View>
+          <Text>Achievement</Text>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -13,6 +16,9 @@ export default AchievementScreen;
 
 
 const styles = StyleSheet.create({
-
+  gradient:{
+        height: '100%',
+        width:'100%',
+  }
 
 });
--- screens/CheckInScreen.js
+++ screens/CheckInScreen.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, Button, TouchableOpacity, TextInput } from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome5";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function CheckInScreen( navigation) {
 
@@ -9,80 +10,75 @@ export default function CheckInScreen( navigation) {
 
 
   return (
-    <View style = {styles.background}>
-      <Text style={styles.headline}>Daily Check-In</Text>
-      <Text style={styles.title}>Mood</Text>
-      <View style={styles.space} />
-      <View style={{ flexDirection:"row" , justifyContent: 'center'}}>
-          <TouchableOpacity>
-              <Icon name="laugh-beam" size={60} color="#9fce68" />
-          </TouchableOpacity>
+    <LinearGradient style= {styles.gradient} colors = {['#C7CEF4', '#DCA3C2']}>
+      <View style = {styles.background}>
+        <Text style={styles.headline}>Daily Check-In</Text>
+        <Text style={styles.title}>Mood</Text>
+        <View style={styles.space} />
+        <View style={{ flexDirection:"row" , justifyContent: 'center'}}>
+            <TouchableOpacity>
+                <Icon name="laugh-beam" size={60} color="#9fce68" />
+            </TouchableOpacity>
+            <View style={styles.space} />
+            <TouchableOpacity>
+                <Icon name="smile-beam" size={60} color="#b1ea6f" />
+            </TouchableOpacity>
+            <View style={styles.space} />
+            <TouchableOpacity>
+                <Icon name="meh" size={60} color="#e9e9e9" />
+            </TouchableOpacity>
+            <View style={styles.space} />
+            <TouchableOpacity>
+                <Icon name="frown" size={60} color="#ffa5c3" />
+            </TouchableOpacity>
+            <View style={styles.space} />
+            <TouchableOpacity>
+                <Icon name="angry" size={60} color="#ed7d8a" />
+            </TouchableOpacity>
+        </View>
+
+        <View style={{alignItems:'center'}}>
           <View style={styles.space} />
-          <TouchableOpacity>
-              <Icon name="smile-beam" size={60} color="#b1ea6f" />
-          </TouchableOpacity>
+          <Text style={styles.title}>Water intake</Text>
           <View style={styles.space} />
-          <TouchableOpacity>
-              <Icon name="meh" size={60} color="#e9e9e9" />
-          </TouchableOpacity>
+
+          <TextInput 
+            keyboardType="numeric"
+            style={styles.input}
+            placeholder='e.g. 2.5'
+            onChangeText={()=> setWater}/>
+
+          <Text style={styles.title}>Amount of exercise</Text>
           <View style={styles.space} />
-          <TouchableOpacity>
-              <Icon name="frown" size={60} color="#ffa5c3" />
-          </TouchableOpacity>
+
+          <TextInput 
+            keyboardType="numeric"
+            style={styles.input}
+            placeholder='e.g. 3'
+            onChangeText={()=> setExercise()}/>
+
+          <Text style={styles.title}>What did you do today?</Text>
           <View style={styles.space} />
-          <TouchableOpacity>
-              <Icon name="angry" size={60} color="#ed7d8a" />
-          </TouchableOpacity>
+
+          <TextInput 
+            multiline
+            style={styles.input2}
+            placeholder='e.g. 3'
+            onChangeText={()=> setExercise()}/>
+        </View>
+          
+
       </View>
-
-        <View style={styles.space} />
-        <Text style={styles.title}>Water intake</Text>
-        <View style={styles.space} />
-
-        <TextInput 
-          keyboardType="numeric"
-          style={styles.input}
-          placeholder='e.g. 2.5'
-          onChangeText={()=> setWater}/>
-
-        <Text style={styles.title}>Amount of exercise</Text>
-        <View style={styles.space} />
-
-        <TextInput 
-          keyboardType="numeric"
-          style={styles.input}
-          placeholder='e.g. 3'
-          onChangeText={()=> setExercise()}/>
-
-        <Text style={styles.title}>What did you do today?</Text>
-        <View style={styles.space} />
-
-        <TextInput 
-          multiline
-          style={styles.input2}
-          placeholder='e.g. 3'
-          onChangeText={()=> setExercise()}/>
-
-        <Text style={styles.title}>Remainder of today's goals or plans:</Text>
-        <View style={styles.space} />
-
-        <TouchableOpacity onPress={() => navigation.navigate('OverviewScreen')}>
-              <Icon name="sign-in-alt" size={60} color="#b1ea6f" />
-          </TouchableOpacity>
-
-    </View>
+    </LinearGradient>
   );
 }
 
 
 
-// export default CheckInScreen;
-
-
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: "#C7CEF4",
+    //backgroundColor: "#C7CEF4",
   },
   headline: {
     textAlign: 'center',
@@ -93,7 +89,7 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     fontSize: 19,
-    backgroundColor: "#7894cf",
+    //backgroundColor: "#7894cf",
   },
   space: {
     width: 20,
@@ -113,5 +109,9 @@ const styles = StyleSheet.create({
     margin:10,
     width:200,
     height:150,
+  },
+  gradient:{
+        height: '100%',
+        width:'100%',
   }
 });
